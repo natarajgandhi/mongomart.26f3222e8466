e079019d7c100ff24a6b4c2e5df80defe7350db4
natarajgandhi <natarajgandhi@gmail.com>
2016-05-08 16:53:25 -0400

    updated mongodb connection to mongolabs

--- mongomart.js
+++ mongomart.js
@@ -52,7 +52,9 @@ var ITEMS_PER_PAGE = 5;
 // Hardcoded USERID for use with the shopping cart portion
 var USERID = "558098a65133816958968d88";
 
-MongoClient.connect('mongodb://localhost:27017/mongomart', function(err, db) {
+//MongoClient.connect('mongodb://localhost:27017/mongomart', function (err, db) {
+MongoClient.connect('mongodb://natarajgandhi:voila123@ds017582.mlab.com:17582/shoppingmart-natarajgandhi', function (err, db) {
+    
     "use strict";
 
     assert.equal(null, err);
